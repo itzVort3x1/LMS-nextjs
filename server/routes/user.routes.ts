a43a9be6375e0aa82d6e661f75a8e158af29af2c
itzVort3x1 <saikaustubh10@gmail.com>
2023-10-04 06:57:02 +0530

    added route authentication and validating route access

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -5,11 +5,12 @@ import {
 	loginUser,
 	logoutUser,
 } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/logout", logoutUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
 
 export default userRouter;
