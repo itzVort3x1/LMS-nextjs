512d288cc739e043c0e440cd8b87aef0f21375dd
itzVort3x1 <saikaustubh10@gmail.com>
2023-10-08 16:53:53 +0530

    added getAll courses route

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -84,3 +84,20 @@ export const getSingleCourse = CatchAsyncError(
 		}
 	}
 );
+
+export const getAllCourses = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const courses = await CourseModel.find().select(
+				"-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+			);
+
+			res.status(200).json({
+				success: true,
+				courses,
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import {
 	editCourse,
+	getAllCourses,
 	getSingleCourse,
 	uploadCourse,
 } from "../controllers/course.controller";
@@ -22,5 +23,6 @@ courseRouter.put(
 );
 
 courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourses);
 
 export default courseRouter;
