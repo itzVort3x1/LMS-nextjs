5485519dbb071366bff48c52411baf675365ff12
itzVort3x1 <saikaustubh10@gmail.com>
2023-10-08 16:43:16 +0530

    added edit course route

--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 const courseRouter = express.Router();
 
 courseRouter.post(
@@ -8,6 +8,13 @@ courseRouter.post(
 	isAuthenticated,
 	authorizeRoles("admin"),
 	uploadCourse
+);
+
+courseRouter.put(
+	"/edit-course/:id",
+	isAuthenticated,
+	authorizeRoles("admin"),
+	editCourse
 );
 
 export default courseRouter;
