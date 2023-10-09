95f31ae0e0216c060edfe2def3716349057235ee
itzVort3x1 <saikaustubh10@gmail.com>
2023-10-09 07:29:12 +0530

    added getCourseByUser route

--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -3,6 +3,7 @@ import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import {
 	editCourse,
 	getAllCourses,
+	getCourseByUser,
 	getSingleCourse,
 	uploadCourse,
 } from "../controllers/course.controller";
@@ -24,5 +25,6 @@ courseRouter.put(
 
 courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
+courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
 
 export default courseRouter;
