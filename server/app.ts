20bb7fb5ee272d090c438a3267f057f8a7dd850b
itzVort3x1 <saikaustubh10@gmail.com>
2023-10-25 07:57:44 +0530

    added layout API's

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,7 @@ import courseRouter from "./routes/course.routes";
 import orderRouter from "./routes/order.routes";
 import notificationRouter from "./routes/notification.routes";
 import analyticsRouter from "./routes/analytics.routes";
+import layoutRouter from "./routes/layout.routes";
 
 //body parser
 app.use(express.json({ limit: "50mb" }));
@@ -29,6 +30,7 @@ app.use("/api/v1", courseRouter);
 app.use("/api/v1", orderRouter);
 app.use("/api/v1", notificationRouter);
 app.use("/api/v1", analyticsRouter);
+app.use("/api/v1", layoutRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
