30999831bb62fed51579d3010df860974449c728
itzVort3x1 <saikaustubh10@gmail.com>
2023-12-16 17:55:30 +0530

    finished courseData and course information

--- client/app/components/Admin/Course/CreateCourse.tsx
+++ client/app/components/Admin/Course/CreateCourse.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
+import CourseInformation from "./CourseInformation";
+import CourseOptions from "./CourseOptions";
+import CourseData from "./CourseData";
 
 type Props = {};
 
@@ -37,7 +40,33 @@ const CreateCourse = (props: Props) => {
 	]);
 	const [courseData, setCourseData] = useState({});
 
-	return <div className="w-full flex min-h-screen"></div>;
+	return (
+		<div className="w-full flex min-h-screen">
+			<div className="w-[80%]">
+				{active === 0 && (
+					<CourseInformation
+						courseInfo={courseInfo}
+						setCourseInfo={setCourseInfo}
+						active={active}
+						setActive={setActive}
+					/>
+				)}
+				{active === 1 && (
+					<CourseData
+						benefits={benefits}
+						setBenefits={setBenefits}
+						prerequisites={prerequisites}
+						setPrerequisites={setPrerequisites}
+						active={active}
+						setActive={setActive}
+					/>
+				)}
+			</div>
+			<div className="w-[20%] mt-[100px] h-screen fixed z-[-1] top-18 right-0">
+				<CourseOptions active={active} setActive={setActive} />
+			</div>
+		</div>
+	);
 };
 
 export default CreateCourse;
